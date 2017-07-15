0e9afb979af705466bc71ceea42e6e66c32e54f9
Matilda Pan <matilda.yipan@gmail.com>
2017-07-15 23:42:18 +1000

    apply logic

--- index.js
+++ index.js
@@ -1,9 +1,104 @@
-const rover = require('./rover.js');
+const Rover = require('./rover.js');
+const stdin = process.openStdin();
+console.log("To terminate this program, please press q\n");
+console.log("Please input the upper-right coordinates of the plateau: ");
 
-function test() {
+var stageType = {
+    waitingForBound : 0,
+    waitingForCoordinate: 1,
+    waitingForCommand: 2
 }
 
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
+var stage  = stageType.waitingForBound;
+var rover;
+
+var bound = {};
+let inputArray;
+
+stdin.addListener("data", function(d) {
+    if (d.toString().trim() == "q"){
+        process.exit();
+    }else {
+        switch (stage){
+            case stageType.waitingForBound:
+                inputArray = d.toString().trim().split(" ");
+                let validateBoundValue = validateBound(inputArray);
+                if(!validateBoundValue){
+                    console.log("Invalidate bound, please enter again:");
+                }else {
+                    bound.xBound = parseInt(inputArray[0]);
+                    bound.yBound = parseInt(inputArray[1]);
+                    rover = new Rover(bound);
+                    stage = stageType.waitingForCoordinate;
+                    console.log("Please input rover position:");
+                }
+                break;
+            case stageType.waitingForCoordinate:
+                inputArray = d.toString().trim().split(" ");
+                let validateCoordinateValue = validateCoordinate(inputArray);
+                if(!validateCoordinateValue){
+                    console.log("Invalid coordinate, please enter again:");
+                }else {
+                    rover.setPosition(inputArray);
+                    let isCoordinateWithinBound = rover.coordinateWithinBound(rover.position);
+                    if(!isCoordinateWithinBound){
+                        console.log('rover position is out of bound, please enter again:');
+                    }else{
+                        stage = stageType.waitingForCommand;
+                        console.log("Please input rover explore instruction:");
+                    }
+                }
+                break;
+            case stageType.waitingForCommand:
+                let instructionStr = d.toString().trim();
+                let moveResult = rover.explore(instructionStr);
+                if(moveResult == 'invalid instruction'){
+                    console.log("Invalid instruction, please enter again:");
+                }
+                else if(moveResult == 'out of bound'){
+                    console.log("Rover is out of bound, please input rover position again:");
+                }else {
+                    console.log("Current rover position: " + rover.position.x + " " + rover.position.y + " " + rover.position.direction);
+                    console.log("Please input rover new position: ");
+                    rover = new Rover(bound);
+                    stage = stage.waitingForCoordinate;
+                    break;
+                }
+
+
+        }
+
+    }
+
 
 });
+
+
+function validateBound(boundArray) {
+    if(boundArray.length != 2){
+        return false;
+    }else {
+        for(let key in boundArray){
+            let boundInt = parseInt(boundArray[key]);
+            if(isNaN(boundInt)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+function validateCoordinate(coordinateArray) {
+    if(coordinateArray.length != 3){
+        return false;
+    }else {
+        let coordinateX = parseInt(coordinateArray[0]);
+        let coordinateY = parseInt(coordinateArray[1]);
+        let direction = coordinateArray[2];
+        if(isNaN(coordinateX) || isNaN(coordinateY)){
+            return false;
+        }
+        let directionCollection = ['E', 'S', 'W', 'N'];
+        return directionCollection.indexOf(direction) > -1;
+    }
+}
--- rover.js
+++ rover.js
@@ -1,11 +1,100 @@
 class Rover{
-	constructor(coordination){
-		this.coordination = coordination;
+	constructor(bound){
+	    this.position = {};
+	    this.bound = bound;
 	}
 
-	move(command){
-	    return 1;
+
+	explore(instructionStr){
+	    let instructionArray = instructionStr.split("");
+	    let validateInstructionResult = this.validateInstruction(instructionArray);
+	    if(!validateInstructionResult){
+	        return 'invalid instruction';
+        }else {
+	        let directionOrder = ['S', 'W', 'N', 'E'];
+            let directionIndex;
+            let newDirectionIndex;
+	        for(let key in instructionArray){
+	            let instruction = instructionArray[key];
+	            switch (instruction){
+                    case 'L':
+                        directionIndex = directionOrder.indexOf(this.position.direction);
+                        newDirectionIndex = directionIndex - 1;
+                        if(newDirectionIndex === -1){
+                            newDirectionIndex = directionOrder.length - 1;
+                        }
+                        this.position.direction = directionOrder[newDirectionIndex];
+                        break;
+                    case 'R':
+                        directionIndex = directionOrder.indexOf(this.position.direction);
+                        newDirectionIndex = directionIndex + 1;
+                        if(newDirectionIndex === directionOrder.length){
+                            newDirectionIndex = 0;
+                        }
+                        this.position.direction = directionOrder[newDirectionIndex];
+                        break;
+                    case 'M':
+                        this.move(this.position.direction);
+                        let isCoordinateWithinBound = this.coordinateWithinBound(this.position);
+                        if(!isCoordinateWithinBound){
+                            return 'out of bound';
+                        }else {
+                            break;
+                        }
+                }
+            }
+            return this.position;
+        }
+
     }
+
+    move(direction){
+	    switch (direction){
+            case 'S':
+                this.position.y -= 1;
+                break;
+            case 'W':
+                this.position.x -= 1;
+                break;
+            case 'N':
+                this.position.y += 1;
+                break;
+            case 'E':
+                this.position.x += 1;
+                break;
+        }
+	}
+
+
+
+
+    setPosition(coordinateArray){
+        let coordinateX = parseInt(coordinateArray[0]);
+        let coordinateY = parseInt(coordinateArray[1]);
+        let direction = coordinateArray[2];
+        this.position = {
+            x: coordinateX,
+            y: coordinateY,
+            direction: direction
+        };
+    }
+
+    coordinateWithinBound(position) {
+        let boundX = this.bound.xBound;
+        let boundY = this.bound.yBound;
+        return (position.x < boundX) && (position.y < boundY);
+    }
+
+    validateInstruction(instructionArray){
+        let instructionCollection = ['L', 'R', 'M'];
+        for(let key in instructionArray){
+            if(instructionCollection.indexOf(instructionArray[key]) === -1){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
 
 module.exports = Rover;
